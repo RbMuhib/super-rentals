3817a912a7ab0dd24b662a68a2444a67321b57bc
RbMuhib <raghibbmuhib@gmail.com>
2019-09-23 07:21:51 -0400

    till search bar by city

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -5,10 +5,11 @@ import {
   visit
 } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
 module('Acceptance | list rentals', function (hooks) {
   setupApplicationTest(hooks);
-
+  setupMirage(hooks);
   test('should show rentals as the home page', async function (assert) {
     await visit('/');
     assert.equal(currentURL(), '/rentals', 'should redirect automatically');
